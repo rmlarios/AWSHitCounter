25c259cb8782be668ae3b7f2cb507ec2a4ebeb84
Miguel Angel Larios Campos <mlariosc7@hotmail.com>
2022-01-15 13:13:59 -0600

    Proyecto Inicia

--- lib/project_aws-stack.ts
+++ lib/project_aws-stack.ts
@@ -1,19 +1,36 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, lambda_layer_awscli, Stack, StackProps } from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class ProjectAwsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'ProjectAwsQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    //define an AWS Lambda Resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    }
+    );
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
     });
 
-    const topic = new sns.Topic(this, 'ProjectAwsTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
+    });
   }
 }
